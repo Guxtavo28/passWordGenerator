6379126de6a6a0774fd132aa4eb652826286456a
Gustavo Herculano <57715068+Guxtavo28@users.noreply.github.com>
2025-02-14 12:04:00 -0300

    Update Gerador.java

--- src/Gerador.java
+++ src/Gerador.java
@@ -2,7 +2,7 @@ package src;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.classfile.constantpool.IntegerEntry;
+import java.io.*;
 
 //Gson  
 import com.google.gson.Gson;
@@ -36,6 +36,7 @@ public class Gerador {
         throws IOException
     {
         dataColect();
+
     }
 
     //Coleta de dados
@@ -53,65 +54,94 @@ public class Gerador {
         System.out.println("Bem vindo ao MASTER BLASTER EXTREME REVELETIONS 2 ULTIMATE gerador de senhas :D");
         System.out.println("Vou fazer algumas perguntas sobre suas preferências para senha");
 
-        System.out.println("Qual tamanho deseja? \n Digite apenas números");
-        passSize = Integer.parseInt(cr.readLine());
+        boolean validInput = false;
 
-    
+        while (!validInput) {
+            System.out.println("Qual tamanho deseja? \nDigite apenas números:");
+            String input = cr.readLine(); // le a entrada do usuário
 
-        System.out.println("Deseja incluir letras maiúsculas e minúsculas? (Por padrão tem apenas minúsculas) \n Responda s ou n");
+            try {
+                passSize = Integer.parseInt(input); // tenta converter para inteiro
+
+                if (passSize < 1) {
+                    System.out.println("É sério??? Como diabos tu vai fazer uma senha com uma quantidade negativa de números? \n Digita apenas um NÚMERO válido");
+                } else {
+                    validInput = true; // Se rolar ele sai do loop
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Ta me tirando? Digita apenas um NÚMERO válido");
+            }
+        }
+
+        validInput = false;
+
+        
         while(loopCheck){
+            System.out.println("Deseja incluir letras maiúsculas e minúsculas? (Por padrão tem apenas minúsculas) \n Responda s ou n");
+
             caseType = inputCheck(cr.readLine());
         }
         loopCheck = true;
-
-
-        System.out.println("Deseja incluir números? \n Responda s ou n");
+        
         while(loopCheck){
+            System.out.println("Deseja incluir números? \n Responda s ou n");
+            
             numbers = inputCheck(cr.readLine());
         }
         loopCheck = true;
         
-        
-        System.out.println("Deseja incluir caracteres especiais? \n Responda s ou n");
         while(loopCheck){
+            System.out.println("Deseja incluir caracteres especiais? \n Responda s ou n");
+
             specialCharacters = inputCheck(cr.readLine());
         }
         loopCheck = true;
 
         boolean askNewAlphabet = true;
-        System.out.println("Deseja incluir caracteres de algum outro alfabeto? \n Responda s ou n");
+       
+        
 
         while(loopCheck){
+            System.out.println("Deseja incluir caracteres de algum outro alfabeto? \n Responda s ou n");
+
             adicionalAlphabet = inputCheck(cr.readLine());
-        }
-        loopCheck = true;
 
-        while (askNewAlphabet){
             if(adicionalAlphabet){
-                System.out.println("Escolha um dos alfabetos:");
-                System.out.println("1-Cirilico \r\n" + "2-Grego \r\n" + "3-Hebraico \r\n" + "4-Arabe \r\n" + "5-Devanagari \r\n" + "6-Bopomofo \r\n" +  
-                                    "7-Hangul \r\n" + "8-Hiragana \r\n" +  "9-Katakana \r\n" + "10-Tailandes \r\n" + "11-Armenio \r\n" + 
-                                    "12-Georgiano \r\n" + "13-Etiope \r\n" + "14-Cherokee \r\n" + "15-Tibetano \r\n" + "16-Runico \r\n" +
-                                    "17-Linear_b \r\n" + "18-Deseret \r\n" + "19-Osmanya \r\n" + "20-Vai \r\n" + "Responda apenas o numero");
-                                    //talvez tenha alguma forma mais inteligente de fazer esse print. acho que com array sei la
-                /*--------------------------------------------------------------------------------------------------------------------------------------------------- */
-            
-                if(Integer.parseInt(cr.readLine())>20 || Integer.parseInt(cr.readLine())<1){
-                    System.out.println("Escolhe só um dos que estão na lista noia");
-                } else{
-                    moreAlphabets.add(verifyAlphabetType(Integer.parseInt(cr.readLine())));
-                    quantityOfadicionalAlphabets = moreAlphabets.size();
+                //verifica se deu um input válido
+                while (!validInput) {
+                    System.out.println("Escolha um dos alfabetos:");
+                    System.out.println("1-Cirilico \r\n" + "2-Grego \r\n" + "3-Hebraico \r\n" + "4-Arabe \r\n" + "5-Devanagari \r\n" + "6-Bopomofo \r\n" +  
+                                        "7-Hangul \r\n" + "8-Hiragana \r\n" +  "9-Katakana \r\n" + "10-Tailandes \r\n" + "11-Armenio \r\n" + 
+                                        "12-Georgiano \r\n" + "13-Etiope \r\n" + "14-Cherokee \r\n" + "15-Tibetano \r\n" + "16-Runico \r\n" +
+                                        "17-Linear_b \r\n" + "18-Deseret \r\n" + "19-Osmanya \r\n" + "20-Vai \r\n" + "Responda apenas o numero");
+                                        //talvez tenha alguma forma mais inteligente de fazer esse print. acho que com array sei la
+                    /*--------------------------------------------------------------------------------------------------------------------------------------------------- */
+                    String input = cr.readLine(); // le a entrada do usuário
+    
+                    try {
+                        passSize = Integer.parseInt(input); // tenta converter para inteiro
+    
+                        if (passSize < 1 || passSize > 20) {
+                            System.out.println("Francamente...");
+                        } else {
+                            validInput = true; // Se rolar ele sai do loop
 
-                    System.out.println("Deseja incluir mais um? Responda s ou n");
-                    if(cr.readLine().equals("n")){
-                        adicionalAlphabet = false;
-                        askNewAlphabet = false;
-                    } else{
-                        System.out.println("fodase deu errado");
+                            moreAlphabets.add(verifyAlphabetType(passSize));
+                            quantityOfadicionalAlphabets = moreAlphabets.size();
+
+                            System.out.println("Temos: " + moreAlphabets.size() + " alfabetos adicionados");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("Ta me tirando? Digita apenas um NÚMERO válido");
                     }
                 }
+                validInput = false;
+
+                System.out.println("Gostaria de adicionar outro? \n responda com s ou n");
+                loopCheck = inputCheck(cr.readLine());
             }
-        }
+        } 
+        loopCheck = true;
     }
 
     //Verifica o tipo do alfabeto
@@ -165,7 +195,7 @@ public class Gerador {
         String finalData = "null";
         
         try {
-            String caminhoJson = "D:/Documentos/Treino Java/json/allCharacters.json";
+            String caminhoJson = "../json/allCharacters.json";
 
             // Le o conteúdo do arquivo JSON
             String jsonContent = new String(Files.readAllBytes(Paths.get(caminhoJson)));
@@ -214,88 +244,4 @@ public class Gerador {
 
         return finalData;
     }
-
-
-
-    public static String jsonReader(String kindOfData, String letterCase){
-        String finalData = "null";
-        
-        try {
-            String caminhoJson = "D:/Documentos/Treino Java/json/allCharacters.json";
-
-            // Le o conteúdo do arquivo JSON
-            String jsonContent = new String(Files.readAllBytes(Paths.get(caminhoJson)));
-            
-            Gson gson = new Gson();
-
-            //Serializa o JSON (Converde de arquivo json pra um JsonObject)
-            JsonObject jsonObject = gson.fromJson(jsonContent, JsonObject.class);
-
-            //Tipo do dado/alfabeto
-            JsonObject dataType = jsonObject.getAsJsonObject(kindOfData);
-
-            //obtendo valor q eu quero
-            finalData = dataType.get(letterCase).getAsString();
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return finalData;
-    }
-
-    public static int  numberOfCharacters(String kindOfData){
-        int finalData = 0;
-
-        try {
-            String caminhoJson = "D:/Documentos/Treino Java/json/allCharacters.json";
-
-            // Le o conteúdo do arquivo JSON
-            String jsonContent = new String(Files.readAllBytes(Paths.get(caminhoJson)));
-            
-            Gson gson = new Gson();
-
-            //Serializa o JSON (Converde de arquivo json pra um JsonObject)
-            JsonObject jsonObject = gson.fromJson(jsonContent, JsonObject.class);
-
-            //Tipo do dado/alfabeto
-            JsonObject dataType = jsonObject.getAsJsonObject(kindOfData);
-
-            //obtendo valor q eu quero
-            finalData = Integer.parseInt(dataType.get("quantidade").getAsString());  
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return finalData;
-    }
-
-    public static String jsonReader(String kindOfData, String letterCase){
-        String finalData = "null";
-        
-        try {
-            String caminhoJson = "D:/Documentos/Treino Java/json/allCharacters.json";
-
-            // Le o conteúdo do arquivo JSON
-            String jsonContent = new String(Files.readAllBytes(Paths.get(caminhoJson)));
-            
-            Gson gson = new Gson();
-
-            //Serializa o JSON (Converde de arquivo json pra um JsonObject)
-            JsonObject jsonObject = gson.fromJson(jsonContent, JsonObject.class);
-
-            //Tipo do dado/alfabeto
-            JsonObject dataType = jsonObject.getAsJsonObject(kindOfData);
-
-            //obtendo valor q eu quero
-            finalData = dataType.get(letterCase).getAsString();
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return finalData;
-    }
-
 }
